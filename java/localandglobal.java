2e731c1638f1c5578dba7e6699a3267c38559967
Mahadev <167230806+Mahadev949@users.noreply.github.com>
2025-06-19 13:52:39 +0530

    Delete java directory

--- java/localandglobal.java
+++
@@ -1,7 +0,0 @@
-class localandglobal{
-	int a=20;
-	public static void main(String args[]){
-		int a=10;
-		System.out.println(a);
-	}
-}
--- java/relational.java
+++
@@ -1,6 +0,0 @@
-class relational{
-	public static void main(String args[]){
-		int a=10,b=20;
-		System.out.println("a<b "+(a<b)+"\na<=b "+(a<=b)+"\na>b "+(a>b)+"\na>=b"+(a>=b)+"\na!=b "+(a!=b));
-	}
-}
